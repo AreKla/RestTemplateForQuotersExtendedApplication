391f2b6de4099b4b5e86400be9a0656b3a5fcd5d
klakd <arkadiusz.klak@gmail.com>
2024-03-11 18:30:16 +0100

    Initial commit

---
+++ src/main/java/com/example/resttemplateforquotersextendedapplication/RestTemplateForQuotersExtendedApplication.java
@@ -0,0 +1,13 @@
+package com.example.resttemplateforquotersextendedapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTemplateForQuotersExtendedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTemplateForQuotersExtendedApplication.class, args);
+	}
+
+}
