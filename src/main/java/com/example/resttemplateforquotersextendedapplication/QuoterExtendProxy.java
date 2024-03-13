bffa00564ec5b61b4cd845b5b41de7f09589fb03
klakd <arkadiusz.klak@gmail.com>
2024-03-13 14:19:51 +0100

    Added error handling in get methods

--- src/main/java/com/example/resttemplateforquotersextendedapplication/QuoterExtendProxy.java
+++ src/main/java/com/example/resttemplateforquotersextendedapplication/QuoterExtendProxy.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
@@ -25,32 +27,32 @@ public class QuoterExtendProxy {
     public List<QuoteExample> showAllQuotes() {
         // GET http://localhost:8080/api
         String uri = url + "/api";
-        return getQuoteExamples(uri);
+        return getQuotes(uri);
     }
 
     public QuoteExample getById(Integer id) {
         // GET http://localhost:8080/api/{id}
         String uri = url + "/api/" + id;
-        return getQuoteExample(uri);
+        return getQuote(uri);
     }
 
     public QuoteExample getRandomQuote() {
         // GET http://localhost:8080/api/random
         String uri = url + "/api/random";
-        return getQuoteExample(uri);
+        return getQuote(uri);
 
     }
 
     public QuoteExample getByParam(Integer param) {
         // GET http://localhost:8080/api?id=param
         String uri = url + "/apiWithRequestParam?id=" + param;
-        return getQuoteExample(uri);
+        return getQuote(uri);
     }
 
     public List<QuoteExample> getByHeader() {
         // GET http://localhost:8080/apiWithHeader
         String uri = url + "/apiWithHeader";
-        return getQuoteExamples(uri);
+        return getQuotes(uri);
     }
 
     public void addQuote(String quote) {
@@ -72,22 +74,36 @@ public class QuoterExtendProxy {
                 String.class);
     }
 
-    private List<QuoteExample> getQuoteExamples(String uri) {
-        ResponseEntity<String> response = restTemplate.exchange(uri,
-                HttpMethod.GET,
-                null,
-                String.class);
-        String json = response.getBody();
-        return mapJsonToTypeReference(json);
+    private List<QuoteExample> getQuotes(String uri) {
+        try {
+            ResponseEntity<String> response = restTemplate.exchange(uri,
+                    HttpMethod.GET,
+                    null,
+                    String.class);
+            String json = response.getBody();
+            return mapJsonToTypeReference(json);
+        } catch (RestClientResponseException exception) {
+            System.out.println(exception.getStatusText() + " " + exception.getStatusCode().value());
+        } catch (RestClientException exception) {
+            System.out.println(exception.getMessage());
+        }
+        return null;
     }
 
-    private QuoteExample getQuoteExample(String uri) {
-        ResponseEntity<String> exchange = restTemplate.exchange(uri,
-                HttpMethod.GET,
-                null,
-                String.class);
-        String json = exchange.getBody();
-        return mapJsonToQuoteExample(json);
+    private QuoteExample getQuote(String uri) {
+        try {
+            ResponseEntity<String> exchange = restTemplate.exchange(uri,
+                    HttpMethod.GET,
+                    null,
+                    String.class);
+            String json = exchange.getBody();
+            return mapJsonToQuoteExample(json);
+        } catch (RestClientResponseException exception) {
+            System.out.println(exception.getStatusText() + " " + exception.getStatusCode().value());
+        } catch (RestClientException exception) {
+            System.out.println(exception.getMessage());
+        }
+        return null;
     }
 
     private static List<QuoteExample> mapJsonToTypeReference(String json) {
